7d3bf74957e4c3a400a7fddb56c2e7ffa6fe5f3e
petehunt <floydophone@gmail.com>
2013-09-04 19:00:37 -0700

    add setup.py, gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-reactify',
+    version='0.1',
+    description='Use React from Django',
+    author='Pete Hunt',
+    author_email='floydophone@gmail.com',
+    py_modules=['reactify']
+)
